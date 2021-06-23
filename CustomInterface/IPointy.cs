08bc781c13a6ec498248ac043c0f3ca09d0befcc
Osazee Isere <oz1.concepts@gmail.com>
2021-06-23 05:45:10 +0100

    initial commit:Custom Interface

---
+++ CustomInterface/IPointy.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomInterface
+{   // The pointy behaviour as read-only property
+    interface IPointy
+    {
+        // A read-write property in an interface would look like this
+        // retType PropName{get;set;}
+        //
+        //while a write-only property in an interface would look like this
+        //retType PropName{set;}
+
+            byte Points { get; }
+        //Implicitly public and abstract 
+        byte GetNumberOfPoint();
+
+    }
+}
--- CustomInterface/Program.cs
+++ CustomInterface/Program.cs
@@ -10,6 +10,7 @@ namespace CustomInterface
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
